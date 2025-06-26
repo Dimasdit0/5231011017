0a5f363e8fde3ac015b6d9c56054af932912e2aa
Dimasdit0 <136093175+Dimasdit0@users.noreply.github.com>
2025-06-26 21:40:56 +0700

    Delete TugasBesar directory

--- TugasBesar/insert_admin.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Jalankan satu kali untuk insert
-include 'config.php';
-$hashed = password_hash("admin123", PASSWORD_DEFAULT);
-$conn->query("INSERT INTO admin (username, password) VALUES ('admin', '$hashed')");
-echo "Admin ditambahkan!";
-?>
